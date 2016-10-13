9e6c22f110c3b8718131cd46c8a8872fd766b818
Hassep <philip.hasse1@marist.edu>
2016-10-13 01:24:24 -0400

    Add files via upload

--- Hasse_Midterm/IterativeN.py
+++ Hasse_Midterm/IterativeN.py
@@ -1,1 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def solveN(N):
+	for k in range(0,10):
+		N = (8/.05**2)*np.log((4*((2*N)**10)+1)/.05)
+		plt.scatter(k,N)
+		print N
+	plt.savefig('2.12.plot.pdf', bbox_inches='tight')
+	plt.show()
+	print N
+
+solveN(1000)
 
