836e56cde401321d9c7d29154b958d22d277c507
Hassep <philip.hasse1@marist.edu>
2016-12-12 22:11:37 -0500

    Add files via upload

--- Hasse-prj/writeup/code/digitsol.py
+++ Hasse-prj/writeup/code/digitsol.py
@@ -1,1 +1,63 @@
+import scipy
+import numpy as np
+import operator
+import csv
+from sklearn import neighbors
 
+
+# loading csv data into numpy array
+def read_data(f, header=True, test=False, rows=0):
+    data = []
+    labels = []
+
+    csv_reader = csv.reader(open(f, "r"), delimiter=",")
+    index = 0
+    for row in csv_reader:
+        index = index + 1
+        if rows > 0 & index > rows:
+            break
+        if header and index == 1:
+            continue
+
+        if not test:
+            labels.append(int(row[0]))
+            row = row[1:]
+
+        data.append(np.array(np.int64(row)))
+    return (data, labels)
+
+def predictKNN(train,labels,test):
+    print 'start knn'
+    knn = neighbors.KNeighborsClassifier()
+    knn.fit(train, labels) 
+    probabilities = knn.predict_proba(test)
+    predictions = knn.predict(test)
+    bestScores = probabilities.max(axis=1)
+    print (sum(bestScores)/len(bestScores))
+    print 'done with knn'
+    return predictions, bestScores
+
+
+if __name__ == '__main__':
+
+    print 'read data!'
+    train, labels = read_data("train.csv", rows=1000)
+    np.save('train_small.npy', train)
+    np.save('labels_small.npy', labels)
+
+    train = np.load('train_small.npy')
+    labels = np.load('labels_small.npy')
+   
+    print 'done reading train'
+    
+
+    test, tmpl = read_data("test.csv", test=True, rows=1000)
+    np.save('test_small.npy', test)
+    np.save('tmpl_small.npy', tmpl)
+
+    test = np.load('test_small.npy')
+    tmpl = np.load('tmpl_small.npy')
+
+    print 'done reading test!'
+    knnPredictions, knnScore = predictKNN(train,labels, test)
+    print 'done!!!'
